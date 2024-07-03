f7db24a12e9cb3c4a65b48e2f5f244f44f9aa5ec
Tanmay Roy <tanmayroy84368@gmail.com>
2024-07-03 20:28:46 +0530

    Fix: nltk downloads

--- DataPreprocessing/datapreprocessing.py
+++ DataPreprocessing/datapreprocessing.py
@@ -1,6 +1,6 @@
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
+# import nltk
+# nltk.download('punkt')
+# nltk.download('stopwords')
 
 from nltk.corpus import stopwords
 from nltk import word_tokenize
